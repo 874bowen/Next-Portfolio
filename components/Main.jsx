12473d6549f1a3b8e1e571a113040da1816ed7d2
Ivan Bowen <bowenivan16@gmail.com>
2022-10-30 21:17:00 +0300

    add main component

---
+++ components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+	return (
+	<div className="w-full h-screen text-center">
+		<div className="max-w-[1240px] w-full h-full mx-auto my-auto p-2 flex justify-center items-center">
+         <div>
+         <p className="uppercase text-sm tracking-widest text-gray-600"> Greatness is not achieved alone</p>
+         <h1 className="py-4 text-gray-700">
+            Hi, I&apos;m <span className="text-[#722410]">Ivan Bowen</span>
+         </h1>
+         </div>
+		</div>
+	</div>
+   );
+};
+
+export default Main;
