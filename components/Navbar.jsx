cf17b79c9d7b30f30104ce4a86fb0499ca54e1f9
Ivan Bowen <bowenivan16@gmail.com>
2022-10-30 20:29:08 +0300

    add navbar links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-// import {AiOutlineClose} from "react-icons/ai";
+import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from "react-icons/ai";
+import {FaGithub, FaLinkedinIn} from "react-icons/fa";
+import {BsFillPersonLinesFill} from "react-icons/bs";
 
 const Navbar = () => {
 	return (
 		<div className="fixed w-full h-20 shadow-xl z-[100]">
 			<div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
 				<Image
-					src="/../public/assets/golo.png"
+					src="/../public/assets/bivan.png"
 					alt="/"
 					width="125"
 					height="30"
@@ -35,21 +37,62 @@ const Navbar = () => {
 							</li>
 						</Link>
 					</ul>
-					<div className="md:hidden">{/* <AiOutlineClose size={25} /> */}</div>
+					<div className="md:hidden"><AiOutlineMenu size={25} /></div>
 				</div>
 			</div>
 			<div className="fixed left-0 top-0 w-full h-screen bg-black/70 ">
 				<div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-100">
-					<div className="">
-						<div className="flex w-full items-center justify-between">
+					<div className="container-fluid">
+						<div className="flex items-center justify-between">
 							<Image
-								src="/../public/assets/golo.png"
+								src="/../public/assets/bivan.png"
 								alt="/"
-								width="125"
+								width="50"
 								height="30"
+								
 							/>
 							<div className="rounded-full shadow-lg shadow-gray-400 p3 cursor-pointer">
-								{/* <AiOutlineClose size={25} /> */}+
+								<AiOutlineClose size={25} />
+							</div>
+						</div>
+						<div className="border-b border-amber-800 my-4">
+							<p className="w-[85
+								5 md:w-[90%] py-4">Let&apos;s help people solve problems together</p>
+						</div>
+					</div>
+					<div className="py-4 flex flex-col">
+						<ul className="uppercase">
+							<Link href="/">
+								<li className="py-4 text-sm">Home</li>
+							</Link>
+							<Link href="/">
+								<li className="py-4 text-sm">About</li>
+							</Link>
+							<Link href="/">
+								<li className="py-4 text-sm">Skills</li>
+							</Link>
+							<Link href="/">
+								<li className="py-4 text-sm">Projects</li>
+							</Link>
+							<Link href="/">
+								<li className="py-4 text-sm">Contact</li>
+							</Link>
+						</ul>
+						<div className="pt-40">
+							<p className="uppercase tracking-widest text-[#722410]">Let&apos;s connect</p>
+							<div className="flex items-center justify-between my-4 w-full">
+								<div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+									<FaLinkedinIn />
+								</div>
+								<div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+									<FaGithub />
+								</div>
+								<div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+									<AiOutlineMail />
+								</div>
+								<div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+									<BsFillPersonLinesFill />
+								</div>
 							</div>
 						</div>
 					</div>
