20acf0c0af619304df6353d024cd0ab19ff0d101
Ivan Bowen <bowenivan16@gmail.com>
2022-10-31 21:28:14 +0300

    add main component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from "react-icons/ai";
+import {FaGithub, FaLinkedinIn} from "react-icons/fa";
+import {BsFillPersonLinesFill} from "react-icons/bs";
 
 const Main = () => {
 	return (
@@ -9,6 +12,27 @@ const Main = () => {
          <h1 className="py-4 text-gray-700">
             Hi, I&apos;m <span className="text-[#722410]">Ivan Bowen</span>
          </h1>
+         <h2 className="py-2 text-gray-700">
+            A Software Developer 
+         </h2>
+         <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+         Enthusiastic Software developer eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of web development and designing and training in Responsive Web Design. Motivated to learn, grow and excel in Technology.
+         </p>
+            <div className="flex items-center justify-between m-auto max-w-[33%] w-full py-4">
+               <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaLinkedinIn />
+               </div>
+               <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaGithub />
+               </div>
+               <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <AiOutlineMail />
+               </div>
+               <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsFillPersonLinesFill />
+               </div>
+            </div>
+	
          </div>
 		</div>
 	</div>
